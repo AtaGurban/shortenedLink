d181349d781cf0c2e2f9aee1ec5335b1e6dc6a2b
Ata Gurban <ata-g@mail.ru>
2025-06-12 11:01:08 +0500

    added share static

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 import * as express from "express";
 import * as http from "http";
 import { Express } from "express";
+import * as path from "path";
 import * as cors from "cors";
 import ErrorHandlingMiddleware from "./middleware/ErrorHandlingMiddleware";
 import sequelize from "./db";
@@ -12,7 +13,7 @@ import { PORT } from "./utils/envConst";
 import * as fileUpload from "express-fileupload"
 const app: Express = express();
 const port: number = parseInt(PORT || "8080", 10);
-
+const source = path.join(__dirname, 'build');
 app.use(cors());
 app.use(express.json());
 app.use(
@@ -23,6 +24,7 @@ app.use(
 );
 app.use(express.urlencoded({ extended: true }));
 app.use("/", router);
+app.use(express.static(source));
 const server = http.createServer(app);
 
 app.use(ErrorHandlingMiddleware);
@@ -35,6 +37,9 @@ const start = async () => {
     server.listen(port, async () => {
       console.log(`Server is running on port ${port}`);
     });
+      app.get("*", async (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
   } catch (error) {
     console.log(error);
   }
